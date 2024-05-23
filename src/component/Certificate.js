bf3e15357d5254f6f76b723feb7c1d3176224aa2
Natee <NateeRudkui@outlook.com>
2024-05-23 15:16:17 +0700

    :white_check_mark: : certificate

--- src/component/Certificate.js
+++ src/component/Certificate.js
@@ -1,6 +1,24 @@
+import { Carousel } from "flowbite-react";
+
 const Certificate = () => {
-    return <h1>hello</h1>;
-  };
-  
-  export default Certificate;
-  
+  return (
+    <div className="w-screen flex justify-center">
+      <div className="h-[600px] w-4/6 my-[50px] bg-red-800 p-4 rounded-lg">
+        <Carousel>
+          <img
+            src="images/github.png"
+            alt="..."
+            className="w-full h-full object-contain"
+          />
+          <img
+            src="images/react.jpg"
+            alt="..."
+            className="w-full h-full object-contain"
+          />
+        </Carousel>
+      </div>
+    </div>
+  );
+};
+
+export default Certificate;
