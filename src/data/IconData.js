770352bd071f61325128b8ac62f4abf5062cd2ed
Natee <NateeRudkui@outlook.com>
2024-07-13 15:52:04 +0700

    :white_check_mark: : data heardskill

---
+++ src/data/IconData.js
@@ -0,0 +1,52 @@
+const IconData = [
+  {
+    img_icon: "./images/Js.png",
+    name_icon: "Javascript",
+  },
+  {
+    img_icon: "./images/HTML.png",
+    name_icon: "HTML",
+  },
+  {
+    img_icon: "./images/CSS.png",
+    name_icon: "CSS",
+  },
+  {
+    img_icon: "./images/react.png",
+    name_icon: "React",
+  },
+  {
+    img_icon: "./images/Tailwind_CSS.png",
+    name_icon: "Tailwind",
+  },
+  {
+    img_icon: "./images/bootstarp.png",
+    name_icon: "Bootstarp",
+  },
+  {
+    img_icon: "./images/node_js.png",
+    name_icon: "NodeJs",
+  },
+  {
+    img_icon: "./images/mongodb.png",
+    name_icon: "MongoDB",
+  },
+  {
+    img_icon: "./images/docker.png",
+    name_icon: "Docker",
+  },
+  {
+    img_icon: "./images/github_icon.png",
+    name_icon: "Github",
+  },
+  {
+    img_icon: "./images/playwright.png",
+    name_icon: "Playwright",
+  },
+  {
+    img_icon: "./images/cypress.png",
+    name_icon: "Cypress",
+  },
+];
+
+export default IconData;
