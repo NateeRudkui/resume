1a370e3cf8bd4b1ed559302caf184407c6d05e98
Natee <NateeRudkui@outlook.com>
2024-07-22 21:58:18 +0700

    :lipstick: จัดพื้นที่ และเพิ่มความสวย

--- src/component/AllSkill.js
+++ src/component/AllSkill.js
@@ -1,11 +1,11 @@
 import AllHeardSkill from "./AllHeardSkill";
 import AllSoftSkill from "./AllSoftSkill";
-import "../style/AllSkill.css"
+import "../style/AllSkill.css";
 
 const AllSkill = () => {
   return (
-    <div className="allskill bg-[#F2F2F2] py-[20px]">
-        <h1>My Hard Skill</h1>
+    <div className="allskill uppercase bg-[#F2F2F2] py-[20px] phone:text-center">
+      <h1 className="">My Hard Skill</h1>
       <AllHeardSkill />
       <h1>My Soft Skill</h1>
       <AllSoftSkill />
--- src/component/SkillIcon.js
+++ src/component/SkillIcon.js
@@ -1,6 +1,6 @@
 const SkillIcon = ({ img_icon, name_icon }) => {
   return (
-    <div className="w-28 h-full mx-[1rem]">
+    <div className="w-28 h-full mx-[1rem] my-[5px]">
       <img src={img_icon} className="h-28 w-28" />
       <div className="text-center text-[1rem] pt-[2px]">{name_icon}</div>
     </div>
