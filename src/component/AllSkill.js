a7bcc94e7d7df6a269d61b46c9090f769f81285e
Natee <NateeRudkui@outlook.com>
2024-07-23 15:08:16 +0700

    :lipstick:

--- src/component/AllSkill.js
+++ src/component/AllSkill.js
@@ -5,9 +5,9 @@ import "../style/AllSkill.css";
 const AllSkill = () => {
   return (
     <div className="allskill uppercase bg-[#F2F2F2] py-[20px] phone:text-center">
-      <h1 className="">My Hard Skill</h1>
+      <h1 className="skill-titel">My Hard Skill</h1>
       <AllHeardSkill />
-      <h1>My Soft Skill</h1>
+      <h1 className="skill-titel">My Soft Skill</h1>
       <AllSoftSkill />
     </div>
   );
